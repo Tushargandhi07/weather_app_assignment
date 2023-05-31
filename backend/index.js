cf8ce3589b4de83bf3c73ec3373b377f3598f3e3
Tushargandhi07 <tushargan7@gmail.com>
2023-05-31 19:14:19 +0530

    signup and login done

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const cors=require("cors")
+const { connection } = require("./config/db");
+const { userRoute } = require("./routes/userRoute");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome")
+})
+
+app.use("/user",userRoute)
+app.listen(process.env.port,async()=>{
+    try {
+        await connection;
+        console.log(`Server is running at port ${process.env.port}`)
+        console.log("Successfully connected to DB");
+    } catch (error) {
+        console.log("Error connecting to DB");
+        console.log(error);
+    }
+})
